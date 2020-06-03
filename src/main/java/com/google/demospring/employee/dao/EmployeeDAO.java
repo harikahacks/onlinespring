17eab30270801e547b9a06841aa48ba3f5e78d38
Bhagya Sree Chanda <bhagyasree.c60@gmail.com>
2020-06-02 22:09:01 -0500

    committing employee miroservice

---
+++ src/main/java/com/google/demospring/employee/dao/EmployeeDAO.java
@@ -0,0 +1,14 @@
+package com.google.demospring.employee.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeDAO {
+	
+	public EmployeeDAO() {
+		System.out.println("Employee DAO");
+		
+		
+	}
+
+}
---
+++ src/main/java/com/google/demospring/employee/services/EmployeeService.java
@@ -0,0 +1,30 @@
+package com.google.demospring.employee.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.demospring.employee.dao.EmployeeDAO;
+import com.google.demospring.employee.helper.EmployeeHelper;
+import com.google.demospring.employee.model.Employee;
+
+@Service
+public class EmployeeService {
+	
+	@Autowired
+	private EmployeeDAO employeeDAO;
+	
+	@Autowired
+	private EmployeeHelper employeeHelper;
+	
+	
+	public EmployeeService() {
+		System.out.println("EmployeeService");
+	}
+	
+	public Employee saveEmployee(Employee employee) {
+		System.out.println(employeeDAO);
+		System.out.println(employeeHelper);
+		return employee;
+	}
+
+}
